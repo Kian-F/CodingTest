a2f9f0786b3d8872ecc215c0986ea30d8d17f8d5
Kian-F <52603628+Kian-F@users.noreply.github.com>
2020-11-04 21:22:04 +1100

    sort main table

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -2,7 +2,7 @@ class PeopleController < ApplicationController
   
 
   def index
-    @people = Person.page(params[:page])
+    @people = Person.page(params[:page]).order(params[:sort])
     # render :json => @people
   end
  
@@ -12,12 +12,11 @@ class PeopleController < ApplicationController
     #Location.my_import(params[:file])
     redirect_to root_url, notice: "Successfuly Imported Data!"
   end
-
+  
   def search
     
     # if params[:q].blank?  
     #   redirect_to(search_url, alert: "Empty field!") and return  
-     @people = Person.where("name LIKE? OR location LIKE? OR species LIKE? OR gender LIKE? OR affiliations LIKE? OR weapon LIKE? OR vehicle LIKE? ","%"  + params[:q].downcase + "%","%"  + params[:q].downcase + "%","%"  + params[:q].downcase + "%","%"  + params[:q].downcase + "%","%"  + params[:q].downcase + "%","%"  + params[:q].downcase + "%","%"  + params[:q].downcase + "%").page(params[:page])
-     
+     @people = Person.where("name LIKE? OR location LIKE? OR species LIKE? OR gender LIKE? OR affiliations LIKE? OR weapon LIKE? OR vehicle LIKE? ","%"  + params[:q].downcase + "%","%"  + params[:q].downcase + "%","%"  + params[:q].downcase + "%","%"  + params[:q].downcase + "%","%"+ params[:q].downcase + "%","%"  + params[:q].downcase + "%","%"  + params[:q].downcase + "%").page(params[:page]).order(params[:sort])
    end
- end
+end
